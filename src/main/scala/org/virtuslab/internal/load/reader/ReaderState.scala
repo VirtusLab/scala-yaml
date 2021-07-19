0c99f8f905396477a9ccf0aecb0d40d3d99c43be
Kamil Podsiadlo <kpodsiadlo@virtuslab.com>
2021-07-19 10:21:13 +0200

    Review fixes

--- src/main/scala/org/virtuslab/internal/load/reader/ReaderState.scala
+++ src/main/scala/org/virtuslab/internal/load/reader/ReaderState.scala
@@ -1,11 +1,8 @@
 package org.virtuslab.internal.load.reader
 
-sealed trait ReaderState:
-  def indent: Int
+sealed trait ReaderState
 case object ReaderState:
-  case object Stream extends ReaderState:
-    override val indent = 0
-  case object Document extends ReaderState:
-    override val indent = 0
+  case object Stream               extends ReaderState
+  case object Document             extends ReaderState
   case class Mapping(indent: Int)  extends ReaderState
   case class Sequence(indent: Int) extends ReaderState
