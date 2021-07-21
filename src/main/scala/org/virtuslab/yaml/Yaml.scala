678974b5af370199e2ec44661041b18818e4ceef
Kamil Podsiadlo <kpodsiadlo@virtuslab.com>
2021-07-21 12:38:37 +0200

    Toplevel extension methods

--- src/main/scala/org/virtuslab/yaml/Yaml.scala
+++ src/main/scala/org/virtuslab/yaml/Yaml.scala
@@ -7,18 +7,17 @@ import org.virtuslab.yaml.internal.load.compose.ComposerImpl
 
 import scala.deriving.Mirror
 
-object Yaml:
-  inline def deriveEncoder[T](using m: Mirror.Of[T]): YamlEncoder[T] = YamlEncoder.derived[T]
-  inline def deriveDecoder[T](using m: Mirror.Of[T]): YamlDecoder[T] = YamlDecoder.derived[T]
+inline def deriveEncoder[T](using m: Mirror.Of[T]): YamlEncoder[T] = YamlEncoder.derived[T]
+inline def deriveDecoder[T](using m: Mirror.Of[T]): YamlDecoder[T] = YamlDecoder.derived[T]
 
-  extension (str: String)
-    def as[T](using c: YamlDecoder[T]): Either[YamlError, T] =
-      for
-        node <- ComposerImpl.compose(str)
-        t <- c.construct(node)
-      yield t
+extension (str: String)
+  def as[T](using c: YamlDecoder[T]): Either[YamlError, T] =
+    for
+      node <- ComposerImpl.compose(str)
+      t    <- c.construct(node)
+    yield t
 
-  extension[T] (t: T)
-    def asYaml(using encoder: YamlEncoder[T]): String =
-      val events = SerializerImpl.toEvents(encoder.asNode(t))
-      PresenterImpl.asString(events)
+extension [T](t: T)
+  def asYaml(using encoder: YamlEncoder[T]): String =
+    val events = SerializerImpl.toEvents(encoder.asNode(t))
+    PresenterImpl.asString(events)
