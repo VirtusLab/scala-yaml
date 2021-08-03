8f7b512d40a38097ef14c1b66e42cb692211e1c5
Łukasz Wroński <lukaszwronski98@gmail.com>
2021-08-03 09:02:35 +0200

    Removed invalid yamls and trying to pull repo if exits

--- tests/downloadYamlConfigs.sh
+++ tests/downloadYamlConfigs.sh
@@ -2,7 +2,6 @@
 
 declare -a repositories=("https://github.com/kubernetes/examples" "https://github.com/kubernetes/minikube" "https://github.com/kubernetes/kubernetes")
 
-rm -rf repositories
 mkdir repositories
 cd repositories
 
@@ -10,7 +9,32 @@ cd repositories
 for i in "${repositories[@]}"
 do
    git clone "$i"
+   exit_code=$?
+   repo_name="${i##*/}"
+   echo $repo_name
+
+   if [ $exit_code -eq 128 ]; then
+     cd $repo_name
+     git pull
+     cd ..
+   else
+     git clone "$i"
+   fi
+
    # or do whatever with individual element of the array
 done
 
+cd ..
 find ./repositories -name '*.yaml' -exec cp -prv '{}' './test-suite/jvm/src/it/resources/yaml/configs/' ';'
+
+LIB_YAML_PATH=""
+
+for f in ./test-suite/jvm/src/it/resources/yaml/configs/*.yaml; do
+    cat $f | $LIB_YAML_PATH >/dev/null
+
+    ret=$?
+    if [ $ret -ne 0 ]; then
+      rm $f
+    fi
+
+done
