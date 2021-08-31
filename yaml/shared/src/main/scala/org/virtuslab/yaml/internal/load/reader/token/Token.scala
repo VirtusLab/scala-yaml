6d9d1744343c28c05348155604e974490330983a
Łukasz Wroński <46607934+lwronski@users.noreply.github.com>
2021-08-31 10:16:16 +0200

    Skip escaping character for double quote scalars (#64)

--- yaml/shared/src/main/scala/org/virtuslab/yaml/internal/load/reader/token/Token.scala
+++ yaml/shared/src/main/scala/org/virtuslab/yaml/internal/load/reader/token/Token.scala
@@ -25,19 +25,6 @@ case object Token:
   final case class Scalar(value: String, scalarStyle: ScalarStyle) extends Token
   case object Scalar:
 
-    def apply(value: String, scalarStyle: ScalarStyle): Scalar =
-      new Scalar(escapeSpecialCharacter(value), scalarStyle)
-
-    def from(value: String): Scalar = value match
-      case s""""$v"""" => Scalar(v, ScalarStyle.DoubleQuoted)
-      case s"'$v'"     => Scalar(v, ScalarStyle.SingleQuoted)
-      case v           => Scalar(v, ScalarStyle.Plain)
-
-    private def escapeSpecialCharacter(scalar: String): String =
-      scalar.flatMap { char =>
-        char match {
-          case '\\'  => "\\\\"
-          case '\n'  => "\\n"
-          case other => other.toString
-        }
-      }
+    def apply(scalar: String, scalarStyle: ScalarStyle): Scalar =
+      val escapedScalar = ScalarStyle.escapeSpecialCharacter(scalar, scalarStyle)
+      new Scalar(escapedScalar, scalarStyle)
