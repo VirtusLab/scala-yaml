dace452caefcc33e494530caa14de8718f209cc3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-12-29 15:17:53 +0100

    Update os-lib to 0.8.0 (#107)

--- project/Deps.scala
+++ project/Deps.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Deps {
   lazy val munit  = "org.scalameta" %% "munit"  % "0.7.29"
-  lazy val osLib  = "com.lihaoyi"   %% "os-lib" % "0.7.8"
+  lazy val osLib  = "com.lihaoyi"   %% "os-lib" % "0.8.0"
   lazy val pprint = "com.lihaoyi"   %% "pprint" % "0.7.1"
 }
