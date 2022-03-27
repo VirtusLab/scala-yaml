2228f972c40878587f20a8c99aab0d36865d1d69
Scala Steward <me@scala-steward.org>
2022-03-28 01:55:10 +0200

    Update pprint to 0.7.3

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,5 +3,5 @@ import sbt._
 object Deps {
   lazy val munit  = "org.scalameta" %% "munit"  % "0.7.29"
   lazy val osLib  = "com.lihaoyi"   %% "os-lib" % "0.8.1"
-  lazy val pprint = "com.lihaoyi"   %% "pprint" % "0.7.2"
+  lazy val pprint = "com.lihaoyi"   %% "pprint" % "0.7.3"
 }
