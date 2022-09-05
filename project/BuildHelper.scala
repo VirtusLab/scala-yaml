79b8827d4c7dbe0ebc191c2a2e64d3e89b0b5576
Łukasz Wroński <lukaszwronski98@gmail.com>
2022-09-05 21:33:26 +0200

    Skip passing scala 3 doc options to scala 2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -3,24 +3,30 @@ import sbt.Keys._
 
 object BuildHelper {
   lazy val docsSettings = Seq(
-    Compile / doc / scalacOptions ++= Seq(
-      "-project",
-      "Scala-yaml",
-      "-siteroot",
-      "docs",
-      "-project-version",
-      version.value,
-      "-project-logo",
-      "docs/logo.svg",
-      "-social-links:" +
-        "github::https://github.com/VirtusLab/scala-yaml," +
-        "twitter::https://twitter.com/VirtusLab",
-      "-project-footer",
-      s"Copyright (c) 2021, VirtusLab",
-      "-source-links:github://VirtusLab/scala-yaml",
-      "-revision",
-      "master"
-    ),
+    Compile / doc / scalacOptions ++= {
+      CrossVersion.binaryScalaVersion(scalaVersion.value) match {
+        case "3" =>
+          Seq(
+            "-project",
+            "Scala-yaml",
+            "-siteroot",
+            "docs",
+            "-project-version",
+            version.value,
+            "-project-logo",
+            "docs/logo.svg",
+            "-social-links:" +
+              "github::https://github.com/VirtusLab/scala-yaml," +
+              "twitter::https://twitter.com/VirtusLab",
+            "-project-footer",
+            s"Copyright (c) 2021, VirtusLab",
+            "-source-links:github://VirtusLab/scala-yaml",
+            "-revision",
+            "master"
+          )
+        case _ => Nil
+      }
+    },
     Compile / doc := {
       val out = (Compile / doc).value
       IO.copyDirectory((Compile / doc / target).value, file("generated-docs"))
