2fc49e8ac0afdcef696a07c9c5a7bdfcf31e2603
Kamil Podsiadło <37124721+kpodsiad@users.noreply.github.com>
2022-09-07 15:40:54 +0200

    build: minor tweaks (#174)
    - use libraries from `Deps.scala`
    - bump local snapshot version
    - add pprint conditionally on local machines to dependencies
    - add spacing between sbt plugins, otherwise Github is unable to resolve conflicts

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,7 +1,9 @@
 import sbt._
 
 object Deps {
-  lazy val munit  = "org.scalameta" %% "munit"  % "0.7.29"
+  lazy val munit  = "org.scalameta" %% "munit"  % "1.0.0-M6"
+
   lazy val osLib  = "com.lihaoyi"   %% "os-lib" % "0.8.1"
+  
   lazy val pprint = "com.lihaoyi"   %% "pprint" % "0.7.3"
 }
