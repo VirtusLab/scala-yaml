d80f94556387c2a7eb5c37bc5a9323a0cc5f05d8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-04-27 11:48:57 +0200

    Update munit to 1.0.0-M7 (#198)

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Deps {
-  lazy val munit = "org.scalameta" %% "munit" % "1.0.0-M6"
+  lazy val munit = "org.scalameta" %% "munit" % "1.0.0-M7"
 
   lazy val osLib = "com.lihaoyi" %% "os-lib" % "0.9.1"
 
