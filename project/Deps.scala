0194c4df0a01707e539b6e60679aff75932f9e2e
Scala Steward <scala_steward@virtuslab.com>
2023-11-07 02:44:39 +0000

    Update os-lib to 0.9.2

--- project/Deps.scala
+++ project/Deps.scala
@@ -3,7 +3,7 @@ import sbt._
 object Deps {
   lazy val munit = "org.scalameta" %% "munit" % "1.0.0-M7"
 
-  lazy val osLib = "com.lihaoyi" %% "os-lib" % "0.9.1"
+  lazy val osLib = "com.lihaoyi" %% "os-lib" % "0.9.2"
 
   lazy val pprint = "com.lihaoyi" %% "pprint" % "0.8.1"
 }
