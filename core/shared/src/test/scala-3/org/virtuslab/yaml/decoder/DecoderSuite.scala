1aa5e3f4629a6d219331ca83c9233b4f23d0d4f1
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-07-21 13:25:53 +0200

    fixed test

--- core/shared/src/test/scala-3/org/virtuslab/yaml/decoder/DecoderSuite.scala
+++ core/shared/src/test/scala-3/org/virtuslab/yaml/decoder/DecoderSuite.scala
@@ -321,7 +321,7 @@ class DecoderSuite extends munit.FunSuite:
       123      -> 321,
       "string" -> "aezakmi",
       true     -> false,
-      5.5f     -> 55.55f
+      5.5f     -> 55.55d
     )
 
     assertEquals(yaml.as[Map[Any, Any]], Right(expected))
