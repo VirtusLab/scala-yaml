072184fecd3a8ccf8fd63610661e9215de56d594
Scala Steward <scala_steward@virtuslab.com>
2026-02-27 03:28:33 +0000

    Update munit to 1.2.3

--- project/Deps.scala
+++ project/Deps.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Deps {
-  lazy val munit = "org.scalameta" %% "munit" % "1.2.2"
+  lazy val munit = "org.scalameta" %% "munit" % "1.2.3"
 
   lazy val osLib = "com.lihaoyi" %% "os-lib" % "0.11.8"
 
